619898526d7bcdddee9b7a8986504060ca3f5577
Bullough <bertbull@8c8590981250.ant.amazon.com>
2018-07-20 11:17:38 -0600

    Initial commit

---
+++ program.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This script serves as my application to obtain the Python Enthusiast Phone Tool award.
+"""
+
+
+def main():
+    print_header()
+
+
+def print_header():
+    print('--------------')
+    print('Hello, Amazon!')
+    print('--------------')
+
+
+if __name__ == '__main__':
+    main()
